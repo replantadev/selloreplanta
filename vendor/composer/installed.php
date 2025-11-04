9c5e492870f5669a040cc1100eeecbe4efb224cc
Drasanvi <javidrasanvi@gmail.com>
2025-11-04 08:13:37 +0100

    feat: add vendor dependencies for auto-update functionality

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'replantadev/sello-replanta',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '86f5118ebf1a485b8be2fd06d3f18728c1238bf6',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'replantadev/sello-replanta' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '86f5118ebf1a485b8be2fd06d3f18728c1238bf6',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'yahnis-elsts/plugin-update-checker' => array(
+            'pretty_version' => 'v5.0',
+            'version' => '5.0.0.0',
+            'reference' => '81be284da76f12c8751b477b2a0fa44364d26f84',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yahnis-elsts/plugin-update-checker',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
